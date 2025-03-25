85160cc9f32bcff5b754fd63ebfc6f1d2f079535
Tomasz Beneś <tbenes@anaconda.com>
2025-03-25 09:36:42 +0000

    enable jar building on windows

--- recipe/build_jpype1.bat
+++ recipe/build_jpype1.bat
@@ -1,1 +1,1 @@
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation -C--global-option=build_ext
+%PYTHON% -m pip install . -vv --no-deps --no-build-isolation -C--global-option=build_ext -C--global-option="--enable-build-jar"
